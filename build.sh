e30d8a5e71cba2ef643a951c88c8053a5c7f4824
Physuru <46228200+Physuru@users.noreply.github.com>
2021-02-14 05:57:10 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+if [ `uname` = Darwin ]; then
+   openssl_path="$(brew --cellar openssl)/$(brew list --versions openssl | tr ' ' '\n' | tail -1)"
+   gcc -o fwdhttpsd *.c "-I${openssl_path}/include" "${openssl_path}/lib/libssl.a" "${openssl_path}/lib/libcrypto.a"
+   
+   exit
+elif ! [ `uname` = Linux ]; then
+   echo "probably unsupported but trying anyway"
+fi
 gcc -o fwdhttpsd *.c -lpthread -lssl -lcrypto
