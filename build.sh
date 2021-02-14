cdb282896b76659bdd72e3f9e8ed1b5b17adb4e0
Physuru <46228200+Physuru@users.noreply.github.com>
2021-02-14 06:04:56 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# depends on libssl and libcrypto
+# install using apt: apt install libssl-dev
+# install using brew: brew install openssl
+# install using pacman: pacman -S openssl
 if [ `uname` = Darwin ]; then
    openssl_path="$(brew --cellar openssl)/$(brew list --versions openssl | tr ' ' '\n' | tail -1)"
    gcc -o fwdhttpsd *.c "-I${openssl_path}/include" "${openssl_path}/lib/libssl.a" "${openssl_path}/lib/libcrypto.a"
